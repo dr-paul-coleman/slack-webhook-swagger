b03f484f8763de9e551641a0fa494a6d15bda456
Paul Coleman <paul.coleman.dba@gmail.com>
2021-08-31 02:18:40 -0600

    First release of code swagger generator

--- app.js
+++ app.js
@@ -2,19 +2,41 @@ const PORT = process.env.PORT || 5000;
 
 const express = require('express');
 const path = require('path');
+const tiny = require('tiny-json-http')
 
 const app = express();
 
 //Set up App
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/Codey'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
 app.get('/', function(req, res){ 
-
     res.render('index', {})
 });
+
+app.post('/webhook', function(req, res){ 
+    
+    const body = req.body;
+
+    // Set up Callback
+    const options = {
+        url: body.url,
+        data: body.data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    tiny.post(options, function(err, response){
+        res.end( err.message || JSON.stringify(response.body,null,2) );
+    });
+    
+});
+
 
 //Run
 app.listen(PORT, function () {
